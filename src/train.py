58a1edf64e5649af13a320f17c19774bea49c9e0
ASHIQUL HASAN SHAKIL <53114821+shakilsustswe@users.noreply.github.com>
2024-02-01 20:00:08 +0600

    Update train.py

--- src/train.py
+++ src/train.py
@@ -68,10 +68,10 @@ data_loader_params = {
 }
 train_loader = torch.utils.data.DataLoader(train_set, **data_loader_params)
 val_loader = torch.utils.data.DataLoader(val_set, **data_loader_params)
-# test_loaders = [torch.utils.data.DataLoader(x, **data_loader_params) for x in test_set]
-for test_data in test_set:
-    print(test_data)  # This can help you see the characteristics of each dataset
-    test_loaders = torch.utils.data.DataLoader(test_data, sampler=SequentialSampler(test_data), **data_loader_params)
+test_loaders = [torch.utils.data.DataLoader(x, **data_loader_params) for x in test_set]
+# for test_data in test_set:
+#     print(test_data)  # This can help you see the characteristics of each dataset
+#     test_loaders = torch.utils.data.DataLoader(test_data, sampler=SequentialSampler(test_data), **data_loader_params)
 
 
 # logs
