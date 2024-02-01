87f8940800cf9e13eebad904724221c134027cb9
ASHIQUL HASAN SHAKIL <53114821+shakilsustswe@users.noreply.github.com>
2024-02-01 14:02:38 +0600

    Update train.py

--- src/train.py
+++ src/train.py
@@ -68,10 +68,10 @@ data_loader_params = {
 }
 train_loader = torch.utils.data.DataLoader(train_set, **data_loader_params)
 val_loader = torch.utils.data.DataLoader(val_set, **data_loader_params)
-# test_loaders = [torch.utils.data.DataLoader(x, **data_loader_params) for x in test_set]
+test_loaders = [torch.utils.data.DataLoader(x, **data_loader_params) for x in test_set]
 
-# For each test set, initialize DataLoader with a SequentialSampler
-test_loaders = [torch.utils.data.DataLoader(x, sampler=SequentialSampler(x), **data_loader_params) for x in test_set]
+# # For each test set, initialize DataLoader with a SequentialSampler
+# test_loaders = [torch.utils.data.DataLoader(x, sampler=SequentialSampler(x), **data_loader_params) for x in test_set]
 
 # logs
 os.makedirs(args.save_path, exist_ok=True)
